9e93a5754f3f22e9f14efd0d96d0495b6cda3892
Roy van de Water <roy.vwater@gmail.com>
2017-12-06 09:00:02 -0800

    1.2 is done

--- 1/second.js
+++ 1/second.js
@@ -1,20 +1,19 @@
 #!/usr/bin/env node
 
-const last = require('lodash/fp/last')
 const size = require('lodash/fp/size')
 const INPUT = require('./input.json')
 
-let lastDigit = parseInt(last(INPUT))
 let sum = 0
 
-for (let i = 0; i < size(INPUT); i++) {
-  const digit = parseInt(INPUT[i])
+for (let i = 0; i < size(INPUT); i += 1) {
+  const digit = parseInt(INPUT[i], 10)
+  const halfSize = size(INPUT) / 2
+  const otherIndex = (i + halfSize) % size(INPUT)
+  const otherDigit = parseInt(INPUT[otherIndex], 10)
 
-  if (lastDigit === digit) {
-    console.log(lastDigit, '===', digit)
+  if (digit === otherDigit) {
     sum += digit
   }
-  lastDigit = digit
 }
 
 console.log('sum', sum)
