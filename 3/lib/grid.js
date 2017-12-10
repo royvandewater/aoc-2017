ddba895aea7405fc44c1a38ebb320ef7ec119570
Roy van de Water <roy.vwater@gmail.com>
2017-12-10 10:01:10 -0700

    Actually, lets do this

--- 3/lib/grid.js
+++ 3/lib/grid.js
@@ -1,18 +1,8 @@
 const bindAll = require('lodash/fp/bindAll')
-const join = require('lodash/fp/join')
-const times = require('lodash/fp/times')
 
 class Grid {
   constructor() {
     bindAll(Object.getOwnPropertyNames(Grid.prototype), this)
-  }
-
-  printNth(n) {
-    return `${n + 1}`
-  }
-
-  toString(n) {
-    return join('', times(this.printNth, n))
   }
 }
 
